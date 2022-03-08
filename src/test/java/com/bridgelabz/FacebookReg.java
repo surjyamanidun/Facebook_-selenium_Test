44fb5b50ae4aaa19e1e4c4af60c2c111795ebd85
surjya <surjyamaniduan@gmail.com>
2022-03-09 01:17:58 +0530

    Added: Functional Test of selenium

---
+++ src/test/java/com/bridgelabz/FacebookReg.java
@@ -0,0 +1,45 @@
+package com.bridgelabz;
+import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import org.testng.annotations.Test;
+
+public class FacebookReg {
+
+    @Test
+    public void test() throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "F:\\driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.facebook.com/");
+
+        driver.findElement(By.linkText("Create New Account")).click();
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.name("firstname")).sendKeys("Surjya");
+
+        Thread.sleep(3000);
+        driver.findElement(By.name("lastname")).sendKeys("Duan");
+        Thread.sleep(3000);
+        driver.findElement(By.name("reg_email__")).sendKeys("surjyamaniduan@gmail.com");
+        Thread.sleep(3000);
+        driver.findElement(By.name("reg_passwd__")).sendKeys("surya");
+        Thread.sleep(3000);
+        driver.findElement(By.name("birthday_day")).sendKeys("16");
+        Thread.sleep(3000);
+        driver.findElement(By.name("birthday_month")).sendKeys("Aug");
+        Thread.sleep(3000);
+        driver.findElement(By.name("birthday_year")).sendKeys("1996");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[contains(text(),'Male')]")).click();
+        Thread.sleep(3000);
+        driver.close();
+    }
+}
+
+
+
